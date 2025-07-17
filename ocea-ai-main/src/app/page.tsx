b4cc659ca3b0eb6b0854a7fcc2673fbbeede11d6
Dekel-E <dekelelimelech@gmail.com>
2025-07-18 01:45:48 +0300

    final

--- ocea-ai-main/src/app/page.tsx
+++ ocea-ai-main/src/app/page.tsx
@@ -6,7 +6,7 @@ import polygon from "@/../public/icons/Polygon 1.png";
 import rectangle from "@/../public/icons/Rectangle 7.png";
 import deco from "@/../public/icons/test.png";
 
-import face from "@/../public/ira.png";
+import face from "@/../public/amigig.gif";
 import AnimatedProgressBars from "@/components/AnimatedProgress";
 import Link from "next/link";
 
